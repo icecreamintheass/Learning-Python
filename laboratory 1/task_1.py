c0714fdac130ee5bf5e1a91c5a6a101d11582c41
icecreamintheass <60441016+icecreamintheass@users.noreply.github.com>
2024-10-19 03:40:15 +0300

    Update task_1.py

--- laboratory 1/task_1.py
+++ laboratory 1/task_1.py
@@ -3,11 +3,11 @@ numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -
 # TODO заменить значение пропущенного элемента средним арифметическим
 
 missing_index = numbers.index(None)
-
-sum_without_none = sum(num for num in numbers if num is not None)
+sum_without_none = 0
+for num in numbers:
+    if num is not None:
+        sum_without_none += num
 
 average = sum_without_none / len(numbers)
-
 numbers[missing_index] = average
-
 print("Измененный список:", numbers)
