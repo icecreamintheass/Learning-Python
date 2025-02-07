dd1c941023ce9a5d186533d2da239bf9c4a2a1fe
icecreamintheass <60441016+icecreamintheass@users.noreply.github.com>
2025-02-07 20:46:18 +0300

    Update task_1 for laboratory 3

--- task_1.py
+++ task_1.py
@@ -1,31 +1,77 @@
 class Book:
-    """ Базовый класс книги. """
-    def __init__(self, name: str, author: str):
-        self.name = name
-        self.author = author
+    def __init__(self, book_id, title, author_name):
+        self.book_id = book_id
+        self._title = title
+        self._author_name = author_name
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def author_name(self):
+        return self._author_name
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return f'Книга "{self.title}"'
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+        return f"Book(book_id={self.book_id}, title='{self.title}', author_name='{self.author_name}')"
 
 
-class PaperBook:
-    def __init__(self, name: str, author: str, pages: int):
-        self.name = name
-        self.author = author
-        self.pages = pages
+class PaperBook(Book):
+    def __init__(self, book_id, title, author_name, page_count):
+        super().__init__(book_id, title, author_name)
+        self._page_count = page_count
 
-    def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if value > 0:
+            self._page_count = value
+        else:
+            raise ValueError("Количество страниц должно быть больше нуля.")
+
+    def __repr__(self):
+        return f"PaperBook(book_id={self.book_id}, title='{self.title}', author_name='{self.author_name}', page_count={self.page_count})"
 
 
-class AudioBook:
-    def __init__(self, name: str, author: str, duration: float):
-        self.name = name
-        self.author = author
-        self.duration = duration
+class AudioBook(Book):
+    def __init__(self, book_id, title, author_name, audio_duration):
+        super().__init__(book_id, title, author_name)
+        self._audio_duration = audio_duration
 
-    def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+    @property
+    def audio_duration(self):
+        return self._audio_duration
+
+    @audio_duration.setter
+    def audio_duration(self, value):
+        if value > 0:
+            self._audio_duration = value
+        else:
+            raise ValueError("Продолжительность аудиокниги должна быть больше нуля.")
+
+    def __repr__(self):
+        return f"AudioBook(book_id={self.book_id}, title='{self.title}', author_name='{self.author_name}', audio_duration={self.audio_duration})"
+
+
+if __name__ == '__main__':
+    paper_book = PaperBook(1, "Марсианские хроники", "Рэй Брэдбери", 288)
+    audio_book = AudioBook(2, "Цветы для Элджернона", "Дэниел Киз", 12.5)
+
+    # Вывод информации о книгах
+    print(paper_book)  # Книга "Марсианские хроники"
+    print(audio_book)  # Книга "Цветы для Элджернона"
+
+    print(repr(paper_book))  # PaperBook(book_id=1, title='Марсианские хроники', author_name='Рэй Брэдбери', page_count=288)
+    print(repr(audio_book))  # AudioBook(book_id=2, title='Цветы для Элджернона', author_name='Дэниел Киз', audio_duration=12.5)
+
+    # Изменение параметров книги
+    paper_book.page_count = 300
+    audio_book.audio_duration = 13.0
+    print(paper_book.page_count)  # 300
+    print(audio_book.audio_duration)  # 13.0
